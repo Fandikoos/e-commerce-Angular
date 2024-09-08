049002c4799e909dbf3c909e73a5d9f90c4fbe54
Fandikoos <danfandos@gmail.com>
2024-09-08 18:04:58 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,44 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { ProductService } from './product.service';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Página de inicio
+  { path: 'products', component: ProductListComponent }, // Lista de productos
+  { path: 'product/:id', component: ProductDetailComponent }, // Detalle de producto
+  { path: 'profile', component: ProfileComponent }, // Perfil de usuario
+  { path: '**', component: ErrorComponent } // Página de error para rutas no encontradas
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    HomeComponent,
+    ErrorComponent,
+    HeaderComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    ProductService
   ],
   bootstrap: [AppComponent]
 })
